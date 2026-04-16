16debe7566cea488ecbb7a8b0671438996546f77
Pragya <pragya0723cph@gmail.com>
2026-04-16 18:56:17 +0530

    Added basic Java looping programs

---
+++ oddNumbers.java
@@ -0,0 +1,18 @@
+
+    import java.util.Scanner;
+
+public class oddNumbers {
+   public static void main(String[] var0) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+
+      for(int i = 2; i <= n; i ++) {
+         if (i % 2 != 0) {
+         System.out.println(i);
+      }
+      }
+
+   }
+}
+    
+
